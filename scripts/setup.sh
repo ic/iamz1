f68258e5470987fc2914e555d84fe851edcd5178
Eric Platon <zaraki@gmx.com>
2021-12-07 03:12:20 +0000

    More comprehensive setup script.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get install pagekite ffmpeg python3-pip
+sudo apt-get install pagekite ffmpeg python3-pip pigpiod libespeak1
 
-pip3 install --user --upgrade --requirement requirements.txt
+sudo systemctl start pigpiod
+sudo systemctl enable pigpiod
+
+pip3 install --user --requirement requirements.txt
+
+echo 'Please use `raspi-config` to set system requirements:'
+echo -e "\t* Activate the I2C interface (dtparam=i2c_arm=on)"
