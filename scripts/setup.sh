cd3e0b92328d1d7e22fcd697770cb9c9761cd8c7
Eric Platon <zaraki@gmx.com>
2021-12-09 02:50:24 +0000

    Setup script extension and improvement.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-sudo apt-get install pagekite ffmpeg python3-pip pigpiod libespeak1
+set -e
+
+sudo apt-get update
+sudo apt-get install -y \
+	ffmpeg \
+	libespeak1
+	pagekite \
+	pigpiod \
+	python3-pip \
 
 sudo systemctl start pigpiod
 sudo systemctl enable pigpiod
 
-pip3 install --user --requirement requirements.txt
+pip3 install --user --quiet --requirement requirements.txt
 
+read -p "Please choose a 3-letter rover name (must be unique): " RNAME
+RNAME=$(echo $RNAME | tr "[:lower:]" "[:upper:]")
+sudo bash -c "echo YAK_ROVER_NAME=$RNAME >> /etc/environment"
+
+echo
 echo 'Please use `raspi-config` to set system requirements:'
 echo -e "\t* Activate the I2C interface (dtparam=i2c_arm=on)"
